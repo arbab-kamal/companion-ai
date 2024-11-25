b27c21af556b56df79c6e5c62486681054457348
wingie125 <arbab.vns123@gmail.com>
2024-11-25 21:07:54 +0530

    test

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,5 @@
 import Stripe from "stripe";
-
 export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
-  apiVersion: "2022-11-15",
+  apiVersion: "2024-11-20.acacia",
   typescript: true,
 });
